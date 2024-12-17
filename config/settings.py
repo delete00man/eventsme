4718c8cdc3a66f316404bfe2a0d1355a93472f3a
Arthus Haulot <59237956+delete00man@users.noreply.github.com>
2024-12-17 14:57:53 +0100

    correction de host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nkx%n7#sypbt4t(e8zz5#rxyb7=y)317^h(6h75k%uyv$+t#*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.241.136.110', '217.0.0.1']
+ALLOWED_HOSTS = ['35.241.136.110', '127.0.0.1']
 
 
 # Application definition
